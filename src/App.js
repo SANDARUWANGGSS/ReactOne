ed189952f9fb6134359a2e35a914e5071cf762c5
Sandaruwan Gamage <sandaruwan@ebuilder.local>
2023-06-07 10:51:30 +0530

    Object destructuring and indexing and id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@
 import './App.css';
 import Main from './Components/Main';
 
+const myData=[
+  {
+    name:"Mahela",
+    city:"Galle",
+    position:"SE",
+    id:1231
+  },
+  {
+    name:"Sanath",
+    city:"Matara",
+    position:"ASE",
+    id:1232
+  },
+  {
+    name:"Sanga",
+    city:"Kandy",
+    position:"SE",
+    id:1233
+  },
+  {
+    name:"Malinga",
+    city:"Galle",
+    position:"SSE",
+    id:1234
+  }
+]
 function App() {
   return (
    <>
@@ -11,8 +37,22 @@ function App() {
         Sandaruwan Gamage
       </h1>
       <p>Welcome to React</p>
-      <Main/>
-      <Main><h3>Kumar Sangakkara</h3></Main>
+      {/* <Main/> */}
+      {/* Normal Method */}
+      {/* {myData.map((ele)=>{
+        return <Main name={ele.name} city={ele.city} position={ele.position}/>;
+      } */}
+
+      {/* Object Destructuring and using index*/}
+      {/* {myData.map(({name,city,position},index)=>{
+        return <Main key={index} name={name} city={city} position={position}/>;
+      } */}
+
+      {/* using index */}
+      {myData.map(({name,city,position,id})=>{
+        return <Main key={id} name={name} city={city} position={position}/>;
+      }
+      )}
     </div>
    </>
     
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,24 @@
-function Main(props)
+// const name="Sandaruwan";
+// const position="SE";
+
+// const myObject = {
+//     name:`${name}`,
+//     position:`${position}`,
+//     city:'Tangalle'
+// }
+
+function Main({children,name,city,position})
 {
+    // console.log(props);
+    // const {city}=myObject //Object Destructuring
     return <div>
         This is a main component
         <ul>
-            <li>{props.children}</li>
+            {/* <li>{props.children}</li> */}
+            <li>{name}</li>
+            <li>{city}</li>
+            <li>{position}</li>
+            {/* <li>{children}</li> */}
         </ul>
         </div>
 }
