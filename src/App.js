1bb7a1139dfa782089a1a0a2bef2d2d5c5430612
Sandaruwan Gamage <sandaruwan@ebuilder.local>
2023-06-09 12:32:48 +0530

    Added some features

--- src/App.js
+++ src/App.js
@@ -21,44 +21,73 @@ let myVar = 'Amila';
 
 
 function App() {
-  const [myVar,setMyVar] = useState({name:"Mahela",
-  city:"Galle",
-  position:"SE",})
+  // const [myVar,setMyVar] = useState({name:"Mahela",
+  // city:"Galle",
+  // position:"SE",})
   const clickHandle = ()=>
 {
-  // myVar='Sanda';
-  setMyVar({...myVar,
-  name:"Malinga",
-  city:"Dambulla"
-});
+  setTimeout(()=>{
+    setInputVal((pre)=>
+    {
+      if(pre<1)
+    {
+      return pre+1
+    }
+    else if(pre<2)
+    {
+      return pre+3
+    }
+    else
+    {
+      return pre+2
+    }
+    }
+    )
+    
+    // console.log('Im Logged')
+    
+  },3000)
+  
 
-setMyDataState([...myDataState,{
-    name:"Pathum",
-    city:"Kaluthara",
-    position:"Cricketer",
-    id:1238,
-    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Mahela_Jayawardene_3.JPG/1200px-Mahela_Jayawardene_3.JPG'
-}])
-  console.log(myVar)
+  // myVar='Sanda';
+//   setMyVar({...myVar,
+//   name:"Malinga",
+//   city:"Dambulla"
+// });
+
+// setMyDataState([...myDataState,{
+//     name:"Pathum",
+//     city:"Kaluthara",
+//     position:"Cricketer",
+//     id:1238,
+//     image:'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Mahela_Jayawardene_3.JPG/1200px-Mahela_Jayawardene_3.JPG'
+// }])
+//   console.log(myVar)
 }
 
- const[myDataState,setMyDataState]=useState(myData);
-console.log(myDataState)
+//  const[myDataState,setMyDataState]=useState(myData);
+ const[inputVal,setInputVal]=useState(0);
 
-  const mainBlock = myDataState.map(({name,city,position,image,id})=>{
-    return <Main key={id} name={name} city={city} position={position} image={image}/>;
+ const inputHandle= (event,data)=>
+ {
+  event.preventDefault()
+  console.log(data)
+  setInputVal(event.target.value)
+ }
+// console.log(myDataState)
+console.log(inputVal)
+// myDataState
+  const mainBlock = myData.map(({name,city,position,image,id},index)=>{
+    return <Main key={id+index} name={name} city={city} position={position} image={image}/>;
   }
-
- 
-
-  )
+)
   return (
   //  <>
    <div>
     <div className='main_container'>
       React Learning
       <h1>
-        {myVar.name}
+        {inputVal}
       </h1>
       <p>Welcome to React</p>
       {/* <Main/> */}
@@ -86,8 +115,20 @@ console.log(myDataState)
         fontSize:"12px",
         border:"1px solid red",
         padding:"7px 12px",marginLeft:"600px",
+        marginBottom:"20px",
         marginRight:"600px"}} onClick={clickHandle}>Click Me</button>
     </div>
+
+        {/* <input 
+        style={{border:"1px solid red",fontSize:"14px", padding:"7px", 
+        marginLeft:"600px", marginBottom:"20px"}} 
+
+        type="text" 
+          placeholder="Type what you want" 
+        onChange={(e)=>{
+          inputHandle(e,inputVal)
+        }}
+          /> */}
     </div>
   //  </>
     
